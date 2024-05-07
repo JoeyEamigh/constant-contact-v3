1de5e90d67101c88f4389176affd3b6707ff347e
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2024-05-07 14:46:16 -0400

    exporting the correct api object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './types';
 export {
-  HttpClient as ConstantContact,
+  Api as ConstantContact,
   type HttpSuccessResponse as ConstantContactSuccessResponse,
   type HttpErrorResponse as ConstantContactErrorResponse,
   type HttpResponse as ConstantContactResponse,
